137790558532ba0a7da4de48684e2449d9e1a8ac
satoutakeshi <sato.takeshi1987@gmail.com>
2019-03-27 10:27:52 +0900

    add map view

---
+++ 02_HalfModalPresentation/02_HalfModalPresentation/02_HalfModalPresentation/MapViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MapViewController.swift
+//  02_HalfModalPresentation
+//
+//  Created by satoutakeshi on 2019/03/27.
+//  Copyright © 2019 Personal Factory. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
