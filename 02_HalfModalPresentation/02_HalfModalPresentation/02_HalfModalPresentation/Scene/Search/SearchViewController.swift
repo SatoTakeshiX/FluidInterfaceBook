a402d050fcefb14aeca676f076ba87300113c7ec
satoutakeshi <sato.takeshi1987@gmail.com>
2019-03-30 15:56:50 +0900

    developping search view con

--- 02_HalfModalPresentation/02_HalfModalPresentation/02_HalfModalPresentation/Scene/Search/SearchViewController.swift
+++ 02_HalfModalPresentation/02_HalfModalPresentation/02_HalfModalPresentation/Scene/Search/SearchViewController.swift
@@ -8,23 +8,66 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
+final class SearchViewController: UIViewController {
+
+    @IBOutlet private weak var tableView: UITableView!
+    @IBOutlet private weak var searchBar: UISearchBar!
+    @IBOutlet private weak var visualEffectView: UIVisualEffectView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func showHeader() {
+        changeHeader(height: 116.0)
     }
-    */
 
+    func hideHeader() {
+        changeHeader(height: 0.0)
+    }
+
+    func changeHeader(height: CGFloat) {
+        tableView.beginUpdates()
+        if let headerView = tableView.tableHeaderView  {
+            UIView.animate(withDuration: 0.25) {
+                var frame = headerView.frame
+                frame.size.height = height
+                self.tableView.tableHeaderView?.frame = frame
+            }
+        }
+        tableView.endUpdates()
+    }
+}
+
+extension SearchViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as? SearchCell else {
+            return UITableViewCell()
+        }
+        switch indexPath.row {
+        case 0:
+            cell.iconImageView.image = UIImage(named: "mappin")
+            cell.titleLabel.text = "おすすめ場所"
+            cell.subTitleLabel.text = "東京タワー"
+            return cell
+        case 1:
+            cell.iconImageView.image = UIImage(named: "tag")
+            cell.titleLabel.text = "お気に入り"
+            cell.subTitleLabel.text = "0件"
+            return cell
+        default:
+            return UITableViewCell()
+        }
+    }
+}
+
+extension SearchViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
